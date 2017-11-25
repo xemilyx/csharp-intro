6a261571cefcfdb99617b6946ed461ae79b6d915
xemilyx <emilybrown@cranstonlibrary.org>
2017-11-25 13:54:33 -0500

    Initial commit

---
+++ C#/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Variables
+{
+    class HelloWorld
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
