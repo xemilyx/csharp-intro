f7527de30b6f9a7333063eb4ee12e8e1b701c8ab
xemilyx <emilybrown@cranstonlibrary.org>
2017-11-27 21:06:23 -0500

    Adding working gitignore and Classes

---
+++ C#/Classes/Classes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Classes.Math;
+
+namespace Classes
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var emily = new Person();
+            emily.FirstName = "Emily";
+            emily.LastName = "Suon";
+            emily.Introduce();
+            
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(2, 3);
+            Console.WriteLine(result);
+        }
+    }
+}
