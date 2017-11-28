2be5c50bc5a3e3c645e0871dd0506883fec1b7e5
xemilyx <emilybrown@cranstonlibrary.org>
2017-11-27 21:08:17 -0500

    Finished section 4 Nonprimitive Types

---
+++ C#/Enums/Enums/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Enums
+{
+
+    public enum ShippingMethod
+    {
+        RegularAirmail = 1,
+        RegisteredAirmail = 2,
+        Express = 3
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+            
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+        }
+    }
+}
---
+++ C#/Strings/Strings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Strings
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var firstName = "Emily";
+            var lastName = "Suon";
+
+            var fullname = firstName + " " + lastName;
+
+            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+
+            var names = new string[3] {"Emily", "Nong", "Leif"};
+            var formattedNames = string.Join(", ", names);
+
+            var text = @"Hi Nong
+Look into the following paths:
+c:\folder1\folder2
+c:\folder3\folder4";
+            
+            Console.WriteLine(text);
+
+        }
+    }
+}
