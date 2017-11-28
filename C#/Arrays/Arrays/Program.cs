8586995218469f1a4f1997bc70f8ba463effb1de
xemilyx <emilybrown@cranstonlibrary.org>
2017-11-27 20:11:59 -0500

    Testing gitignore

---
+++ C#/Arrays/Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new int[3];
+            numbers[0] = 1;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var names = new string[3] {"Emily", "Nong", "Leif"};
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
+        }
+    }
+}
